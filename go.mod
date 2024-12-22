788bc1127bd66f0dfe6ac645d02c4db9efbed371
ruslan709 <77974170+ruslan709@users.noreply.github.com>
2024-12-22 16:32:32 +0300

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ruslan709/yandex_calculate
+
+go 1.23.2
+
+require github.com/pashapdev/calc_go v0.0.0-20241110120054-6bfe8a604cd6
---
+++ yandex_calculate/cmd/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ruslan709/yandex_calculate/yandex_calculate/internal/application"
+)
+
+func main() {
+	application.RunServer()
+}
