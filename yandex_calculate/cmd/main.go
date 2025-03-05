23475465b37a585e1b4f592f3b89a4ae3ed0701b
ruslan709 <77974170+ruslan709@users.noreply.github.com>
2025-03-05 22:02:28 +0300

    Delete yandex_calculate directory

--- yandex_calculate/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/ruslan709/yandex_calculate/yandex_calculate/internal/application"
-)
-
-func main() {
-	application.RunServer()
-}
--- yandex_calculate/pkg/calculation/errors.go
+++
@@ -1,8 +0,0 @@
-package calculation
-
-import "errors"
-
-var (
-	ErrInvalidExpression = errors.New("invalid expression")
-	ErrDivisionByZero    = errors.New("division by zero")
-)
